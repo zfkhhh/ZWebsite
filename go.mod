71015ea7646d58020366b0fc23c68b384c1a3b58
zfkhhh <zfk7213@qq.com>
2022-01-24 21:07:40 +0800

    add mian.go\go-swagger

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module ZWebsite
 go 1.16
 
 require (
+	github.com/gin-contrib/cors v1.3.1
 	github.com/gin-gonic/gin v1.7.7
 	github.com/go-redis/redis v6.15.9+incompatible
 	github.com/lestrrat-go/file-rotatelogs v2.4.0+incompatible
@@ -10,6 +11,8 @@ require (
 	github.com/pkg/errors v0.8.1
 	github.com/satori/go.uuid v1.2.0
 	github.com/spf13/viper v1.10.1
+	github.com/swaggo/files v0.0.0-20210815190702-a29dd2bc99b2
+	github.com/swaggo/gin-swagger v1.4.0
 	go.uber.org/zap v1.17.0
 	gorm.io/driver/mysql v1.2.3
 	gorm.io/gorm v1.22.5
---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"ZWebsite/dao"
+	"ZWebsite/pkg/logger"
+	"ZWebsite/pkg/setting"
+	"ZWebsite/routers"
+	"context"
+	"fmt"
+
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
+
+
+	"github.com/gin-contrib/cors"
+)
+
+
+func main() {
+	ctx := context.TODO()
+
+	logger.SetUp()
+	dao.InitMysql()
+	dao.InitRedis()
+
+	logger.For(ctx).Info("Starting server ......")
+
+
+	r := routers.InitRouter()
+
+	r.Use(cors.Default())
+
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+	_ = r.Run(fmt.Sprintf(":%s", setting.Setting.ServicePort))
+}
